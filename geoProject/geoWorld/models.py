d5b6dba4a2a4891146e593179e82d205ad206719
Galdanwing <antoinevanderhorst@proton.me>
2023-12-21 14:09:02 +0100

    Add initial models
    Looked at the data and this seems like a fine first approach.

--- geoProject/geoWorld/models.py
+++ geoProject/geoWorld/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Municipality(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    geometry = models.MultiPolygonField()
