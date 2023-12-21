32eb64487f666c7c6653186a750e1ba20b91d40a
Galdanwing <antoinevanderhorst@proton.me>
2023-12-21 14:55:10 +0100

    Add rich for progress checking

--- geoProject/geoWorld/management/commands/ingest_data.py
+++ geoProject/geoWorld/management/commands/ingest_data.py
@@ -1,12 +1,13 @@
 import geopandas
 from django.contrib.gis.geos import GEOSGeometry
 from django.core.management import BaseCommand
+from rich.progress import Progress
 
 from geoWorld.models import Municipality
 
 
 class Command(BaseCommand):
-    help = "Ingest geo data from dutch muncipalities into database"
+    help = "Ingest geo data from Dutch municipalities into the database"
 
     def add_arguments(self, parser):
         parser.add_argument("file_name", nargs=1, type=str)
@@ -15,6 +16,16 @@ class Command(BaseCommand):
         path_to_file = options["file_name"][0]
         gdf = geopandas.read_file(path_to_file)
 
-        for index, row in gdf.iterrows():
-            geos_multipolygon = GEOSGeometry(row["geometry"].wkt)
-            Municipality.objects.create(name=row["name"], geometry=geos_multipolygon)
+        total_rows = len(gdf)
+
+        with Progress() as progress:
+            task = progress.add_task("[cyan]Processing...", total=total_rows)
+
+            for index, row in gdf.iterrows():
+                geos_multipolygon = GEOSGeometry(row["geometry"].wkt)
+                Municipality.objects.create(name=row["name"], geometry=geos_multipolygon)
+
+                # Update progress
+                progress.update(task, advance=1)
+
+        self.stdout.write(self.style.SUCCESS("Data ingestion completed successfully."))
